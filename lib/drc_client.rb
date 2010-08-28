1793ff586b0cff89b85cf7093675aedb3f621c48
Dwayne Macgowan <dwaynemac@gmail.com>
2010-08-28 12:17:15 -0300

    removed auth_attribute added :require_local_user

--- lib/drc_client.rb
+++ lib/drc_client.rb
@@ -9,8 +9,9 @@ module DRCClient
   @@drc_config = {
     :user_model => nil,
     :drc_user_column => nil,
-    :auth_attribute_name => nil,
-    :required_access_level => nil # none, student, teacher, director, federation_president
+    :local_user_id_column => nil,
+    :require_local_user => nil,
+    :require_access_level => nil # none, student, teacher, director, federation_president
   }
 
   def self.filter(opts={})
@@ -26,9 +27,10 @@ module DRCClient
     @@base_url = configuration.delete(:base_url)
     @@drc_config[:user_model] = configuration.delete(:user_model)
     @@drc_config[:drc_user_column] = configuration.delete(:drc_user_column) || :drc_user
-    @@drc_config[:auth_attribute_name] = configuration.delete(:auth_attribute_name)
-    @@drc_config[:required_access_level] = configuration.delete(:required_access_level)
-    configuration.merge!({:cas_base_url => @@base_url, :extra_attributes_session_key => :cas_extra_attributes})
+    @@drc_config[:require_local_user] = configuration.delete(:require_local_user)
+    @@drc_config[:require_access_level] = configuration.delete(:require_access_level)
+    @@drc_config[:local_user_id_column] = configuration.delete(:local_user_id_column) || :id
+    configuration.merge!({:cas_base_url => @@base_url})
     CASClient::Frameworks::Rails::Filter.configure(configuration)
   end
 
@@ -58,32 +60,29 @@ module DRCClient
     # checks if DRC credentials have access to local application
     def allowed_drc_user?
       return false if !logged_in_drc?
-      # by default having a drc_user is enough
       allowed = true
-      if DRCClient.config[:required_access_level]
-        allowed = true # TODO check drc_user has sufficient access_level
-      elsif DRCClient.config[:auth_attribute_name]
-        allowed =  (!session[:cas_extra_attributes].blank? && session[:cas_extra_attributes][DRCClient.config[:auth_attribute_name]].to_s == "true")
-      end
+      # by default having a drc_user is enough
+      allowed &&= drc_user_has_local_user? if DRCClient.config[:require_local_user]
+      allowed &&= true if DRCClient.config[:require_access_level]
       return allowed
     end
 
     # finds local user that matches logged in DRC user
-    # return user's id
+    # return user's id or nil if none found
     def get_local_user_id
       return nil if DRCClient.config[:user_model].nil?
       return nil unless logged_in_drc?
-      user = DRCClient.config[:user_model].find(:first, :conditions => { DRCClient.config[:drc_user_column] => current_drc_user})
+      user = DRCClient.config[:user_model].find(:first, :select => DRCClient.config[:local_user_id_column], :conditions => { DRCClient.config[:drc_user_column] => current_drc_user})
       if user
-        return user[:id]
+        return user[DRCClient.config[:local_user_id_column].to_sym]
       else
         return nil
       end
     end
 
-    # checks if cas_user matches any padma_user
+    # checks if drc_user matches any local application user.
     def drc_user_has_local_user?
-      return get_local_user.nil?
+      return !get_local_user_id.nil?
     end
 
     # returns login url to DRC server.
